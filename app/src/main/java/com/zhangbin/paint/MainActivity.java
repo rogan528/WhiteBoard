b5b4cee1a419a432fa6471a059132d8d012b59df
rogan528 <3097940262@qq.com>
2019-05-13 21:48:53 +0800

    删除部分代码

--- app/src/main/java/com/zhangbin/paint/MainActivity.java
+++ app/src/main/java/com/zhangbin/paint/MainActivity.java
@@ -36,12 +36,7 @@ import java.util.ArrayList;
 public class MainActivity extends Activity implements View.OnClickListener, MediaPlayer.OnPreparedListener {
 
     private String url = "https://www.baidu.com/";
-    private Button mOpen;//打开
-    private LinearLayout mBottom;
     private Button mJxNext;//下一步
-    private EditText mPaintSize;//设置画笔大小
-    private EditText mEraserSize;//设置橡皮大小
-    private EditText mPaintColor;//设置颜色
     private GraffitiView tuyaView;//自定义涂鸦板
     private int screenWidth;
     private int screenHeight;
@@ -73,32 +68,17 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
         mContext = this;
         initView();
         initData();
-        //initAssetsData();
-        initListener();
         initDragView();
 
-    }
-
-    private void initAssetsData() {
-        String input = Util.readFileFromAssets(this, "LiveClient.json");
-        Gson gson = new Gson();
-        listOrderBean = gson.fromJson(input, new TypeToken<ArrayList<OrderBean>>() {
-        }.getType());
-        //drawManger = new DrawManger(tuyaView, mWebView);
-        drawManger.setListorderBean(listOrderBean);
     }
     /**
      * 初始化控件
      */
     private void initView() {
-        mPaintSize = findViewById(R.id.et_paint_size);//设置画笔大小
-        mEraserSize = findViewById(R.id.et_eraser_size);//设置橡皮大小
-        mPaintColor = findViewById(R.id.et_paint_color);//设置颜色
-        mOpen = findViewById(R.id.open);
-        mBottom = findViewById(R.id.ll_bottom);
         mTextView = findViewById(R.id.textView);
         mJxNext = findViewById(R.id.jx_next);
         pptLayout =  findViewById(R.id.pptLayout);
+        mJxNext.setOnClickListener(this);
     }
 
     /**
@@ -143,8 +123,6 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
         }.getType());
         orderDrawManger.setListorderBean(listOrderBean);
         updateLayout();
-        mOpen.setOnClickListener(this);
-        initSetting();
     }
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -191,90 +169,6 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
 
         }
     }
-    private void initSetting() {
-        mPaintSize.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!"".equals(mPaintSize.getText().toString().trim())) {
-                    paintSizeValue = Float.parseFloat(mPaintSize.getText().toString().trim());
-                    tuyaView.setPaintSize(paintSizeValue);
-                }
-            }
-        });
-        mEraserSize.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!"".equals(mEraserSize.getText().toString().trim())) {
-                    eRaserSizeValue = Float.parseFloat(mEraserSize.getText().toString().trim());
-                    tuyaView.setEraserSize(eRaserSizeValue);
-                }
-            }
-        });
-        mPaintColor.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!"".equals(mPaintColor.getText().toString().trim()) && mPaintColor.getText().toString().trim().length() == 7) {
-                    mPaintColorValue = mPaintColor.getText().toString().trim();
-                    tuyaView.setPaintColor(Color.parseColor(mPaintColorValue));
-                } else {
-                    tuyaView.setPaintColor(Color.parseColor("#DC143C"));
-                }
-            }
-        });
-    }
-
-    /**
-     * 监听事件
-     */
-    private void initListener() {
-        //撤销
-        findViewById(R.id.btn_undo).setOnClickListener(this);
-        //还原
-        findViewById(R.id.btn_redo).setOnClickListener(this);
-        //清空
-        findViewById(R.id.btn_clear).setOnClickListener(this);
-        //画笔
-        findViewById(R.id.btn_paint).setOnClickListener(this);
-        //橡皮
-        findViewById(R.id.iv_reaserstyle).setOnClickListener(this);
-        //画圆
-        findViewById(R.id.btn_drawcycle).setOnClickListener(this);
-        //画方形
-        findViewById(R.id.btn_drawrec).setOnClickListener(this);
-        //箭头
-        findViewById(R.id.btn_drawarrow).setOnClickListener(this);
-        mJxNext.setOnClickListener(this);
-    }
 
     /**
      * 轮询播放
@@ -293,100 +187,6 @@ public class MainActivity extends Activity implements View.OnClickListener, Medi
                 //drawManger.NextOrder().ExecuteOrder();
                 orderDrawManger.NextOrder().ExecuteOrder();
                 break;
-            case R.id.open:
-                aboutOpenSetting();
-                break;
-            //画笔
-            case R.id.btn_paint:
-                aboutPaintStyleSetting();
-                break;
-            //橡皮
-            case R.id.iv_reaserstyle:
-                aboutReaserStyleSetting();
-                break;
-            //撤销按钮
-            case R.id.btn_undo:
-                tuyaView.undo();
-                break;
-            //还原按钮
-            case R.id.btn_redo:
-                tuyaView.redo();
-                break;
-            //清除按钮 重做
-            case R.id.btn_clear:
-                //Toast.makeText(MainActivity.this,"清除按钮",Toast.LENGTH_SHORT).show();
-                tuyaView.clear();
-                //恢复成画笔状态
-                tuyaView.setSrcBitmap(null);
-                paintStyleSettingDesc(select_paint_style_paint, true);
-                tuyaView.drawGraphics(DRAW_PATH);
-                break;
-            //以下为画图形状按钮
-            case R.id.btn_drawarrow:
-                tellPaintStyleAndSetDrawGraphics(DRAW_ARROW, select_paint_style_paint);
-                break;
-            case R.id.btn_drawrec:
-                tellPaintStyleAndSetDrawGraphics(DRAW_RECTANGLE, select_paint_style_paint);
-                break;
-            case R.id.btn_drawcycle:
-                tellPaintStyleAndSetDrawGraphics(DRAW_CIRCLE, select_paint_style_paint);
-                break;
-
-
         }
     }
-
-
-    //判断画笔样式并切换画图样式
-    private void tellPaintStyleAndSetDrawGraphics(int drawArrow, int select_paint_style_paint) {
-        if (isPaint) {
-            tuyaView.drawGraphics(drawArrow);
-        } else {//当前为橡皮擦
-            tuyaView.selectPaintStyle(select_paint_style_paint);
-            tuyaView.drawGraphics(drawArrow);
-            isPaint = true;
-        }
-    }
-
-
-    //打开
-    private void aboutOpenSetting() {
-        if (isOpen) {//
-            isOpen = false;
-            mOpen.setText("打开");
-            mBottom.setVisibility(View.GONE);
-        } else {
-            isOpen = true;
-            mOpen.setText("关闭");
-            mBottom.setVisibility(View.VISIBLE);
-        }
-    }
-
-    //画笔样式设置
-    private void aboutPaintStyleSetting() {
-        paintStyleSettingDesc(select_paint_style_paint, true);
-        tuyaView.drawGraphics(DRAW_PATH);
-    }
-
-    //橡皮样式设置
-    private void aboutReaserStyleSetting() {
-        reaserStyleSettingDesc(select_paint_style_eraser, false);
-    }
-
-    //画笔样式设置详情
-    private void reaserStyleSettingDesc(int paintStyle, boolean styleTarget) {
-        tuyaView.selectPaintStyle(paintStyle);
-        isPaint = styleTarget;
-    }
-
-    //画笔样式设置详情
-    private void paintStyleSettingDesc(int paintStyle, boolean styleTarget) {
-        //mPaintStyle.setBackgroundResource(paintStyleResouce);
-        tuyaView.selectPaintStyle(paintStyle);
-        isPaint = styleTarget;
-    }
-
-
-
-
 }
